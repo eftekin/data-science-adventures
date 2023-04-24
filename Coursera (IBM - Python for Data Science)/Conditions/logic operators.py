29e04464c8f60ee9da96a2200815f4218bfae23a
eftekin <75522456+eftekin@users.noreply.github.com>
2023-04-25 00:37:41 +0300

    practice logic operators

---
+++ Coursera (IBM - Python for Data Science)/Conditions/logic operators.py
@@ -0,0 +1,6 @@
+albumDate = 1981
+
+if albumDate < 1980 or albumDate == 1991 or albumDate == 1993:
+    print("This album came out in year", albumDate)
+else:
+    print("This album is not registered in the system.")
