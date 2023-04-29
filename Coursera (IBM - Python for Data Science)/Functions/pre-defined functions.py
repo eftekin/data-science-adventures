724bb47e9258417c0b2e9969d4120ec228edcfc9
eftekin <75522456+eftekin@users.noreply.github.com>
2023-04-29 03:31:44 +0300

    Create pre-defined functions.py

---
+++ Coursera (IBM - Python for Data Science)/Functions/pre-defined functions.py
@@ -0,0 +1,4 @@
+album_ratings = [10.0, 8.5, 9.5, 7.0, 7.0, 9.5, 9.0, 9.5]
+print(album_ratings)
+print(sum(album_ratings))  # adds all the elements in a list or tuple
+print(len(album_ratings))  # returns the length of a list or tuple
