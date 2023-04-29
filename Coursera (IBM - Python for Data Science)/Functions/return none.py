c9f8ed0f15d1abf93b3ed4ef3408973c2c0f3b11
eftekin <75522456+eftekin@users.noreply.github.com>
2023-04-29 03:22:26 +0300

    Create return none.py

---
+++ Coursera (IBM - Python for Data Science)/Functions/return none.py
@@ -0,0 +1,16 @@
+def MJ():
+    print("Michael Jackson")
+
+
+def MJ1():
+    print("Michael Jackson")
+    return None
+
+
+MJ()
+MJ1()
+
+print(MJ())
+print(MJ1())
+
+# None is the default return statement.
