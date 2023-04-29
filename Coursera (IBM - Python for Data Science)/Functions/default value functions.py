5b20f31965c126be70dc6dff63b5953cc7f6e82a
eftekin <75522456+eftekin@users.noreply.github.com>
2023-04-29 03:52:02 +0300

    Create default value functions.py

---
+++ Coursera (IBM - Python for Data Science)/Functions/default value functions.py
@@ -0,0 +1,9 @@
+def isGoodRating(rating=4):
+    if rating < 7:
+        print("this album sucks it's rating is ", rating)
+    else:
+        print("this album is good it's rating is ", rating)
+
+
+isGoodRating()
+isGoodRating(10)
