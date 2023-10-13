71ec5c4d55f0bc983d42ed266b1c3cfa8b8ae311
Mustafa Eftekin <m_eftekin@hotmail.com>
2023-10-13 15:35:46 +0300

    update project with if-else

--- Codecademy (Data Scientist Machine Learning Career Path)/Learn Python/Fundementals/Medical Insurance Project.py
+++ Codecademy (Data Scientist Machine Learning Career Path)/Learn Python/Fundementals/Medical Insurance Project.py
@@ -1,7 +1,19 @@
+def analyze_smoker(smoker_status):
+    if smoker_status == 1:
+        print("To lower your cost, you should consider quitting smoking.")
+    else:
+        print("Smoking is not an issue for you.")
+
+
 def calculate_insurance_cost(name, age, sex, bmi, num_of_children, smoker):
-  estimated_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
-  print("The estimated insurance cost for " + name + " is "  + str(estimated_cost) + " dollars")
-  return estimated_cost
+    estimated_cost = 250*age - 128*sex + 370*bmi + \
+        425*num_of_children + 24000*smoker - 12500
+    print("The estimated insurance cost for " + name +
+          " is " + str(estimated_cost) + " dollars")
+    analyze_smoker(smoker)
+    print("")
+    return estimated_cost
+
 
 # Estimate Maria's insurance cost
 maria_insurance_cost = calculate_insurance_cost("Maria", 28, 0, 26.2, 3, 0)
