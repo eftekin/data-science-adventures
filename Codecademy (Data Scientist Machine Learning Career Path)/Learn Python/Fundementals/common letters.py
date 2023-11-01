b6f380a109d0e7dc711da836e04793b4dc0ffe9d
Mustafa Eftekin <m_eftekin@hotmail.com>
2023-11-01 20:43:16 +0300

    Create common letters.py

---
+++ Codecademy (Data Scientist Machine Learning Career Path)/Learn Python/Fundementals/common letters.py
@@ -0,0 +1,9 @@
+def common_letters(string_one, string_two):
+    common = []
+    for letter in string_one:
+        if (letter in string_two) and not (letter in common):
+            common.append(letter)
+    return common
+
+
+print(common_letters("Pizza", "Banana"))
