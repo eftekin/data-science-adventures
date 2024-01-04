d5beea7e3d786a98772cc06c9c9325e85ab19c9f
Mustafa Eftekin <m_eftekin@hotmail.com>
2024-01-04 23:05:41 +0300

    feat: create '2- Sum and Div.py'

---
+++ 140 Basic Programs/2- Sum and Div.py
@@ -0,0 +1,22 @@
+# Write a Python program to do arithmetical operations addition and division.
+
+
+def addition(a, b):
+    c = a + b
+    return c
+
+
+def division(a, b):
+    c = a / b
+    return c
+
+
+num1 = float(input("Please enter the first number for addition: "))
+num2 = float(input("Please enter the second number for addition: "))
+
+print("Sum of two numbers: " + str(addition(num1, num2)))
+
+num3 = float(input("Please enter the first number for division: "))
+num4 = float(input("Please enter the second number for division: "))
+
+print("Division of two numbers: " + str(addition(num1, num2)))
