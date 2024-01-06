f0c6ff3c9a40b640240e2c69a21bf776ca6c8809
Mustafa Eftekin <m_eftekin@hotmail.com>
2024-01-06 22:34:40 +0300

    feat: create '3- Triangle Area Calculator.py'

---
+++ 140 Basic Programs/3- Triangle Area Calculator.py
@@ -0,0 +1,11 @@
+# Write a Python program to find the area of a triangle.
+
+# Input the base and height from the user
+base = float(input("Enter the base of triangle: "))
+height = float(input("Enter the height of triangle: "))
+
+# Calculate the area of the triangle
+area = 0.5 * base * height
+
+# Print the area
+print("The area of the triangle is " + str(area))
