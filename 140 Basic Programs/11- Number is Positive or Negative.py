4fa4c014d4074865f74714bae4367f8644dbc644
Mustafa Eftekin <m_eftekin@hotmail.com>
2024-01-14 01:03:56 +0300

    feat: create '11- Number is Positive or Negative.py'

---
+++ 140 Basic Programs/11- Number is Positive or Negative.py
@@ -0,0 +1,12 @@
+# Write a Python Program to Check if a Number is Positive, Negative or Zero.
+
+number = float(input("Enter a number: "))
+
+if number > 0:
+    print("Number is positive.")
+elif number == 0:
+    print("Number is zero")
+else:
+    print("Number is negative")
+    
+    
