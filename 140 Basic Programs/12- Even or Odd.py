68acfa00b06a566d9eb1f6a7a30a69c79ea14e91
Mustafa Eftekin <m_eftekin@hotmail.com>
2024-01-15 14:47:44 +0300

    feat: create '12- Even or Odd.py'

---
+++ 140 Basic Programs/12- Even or Odd.py
@@ -0,0 +1,8 @@
+# Write a Python Program to Check if a Number is Odd or Even.
+
+num = float(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
