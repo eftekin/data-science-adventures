38a0b9c5187beddd5aa1a466dc95fbce8b12279e
Mustafa Eftekin <m_eftekin@hotmail.com>
2024-02-11 22:21:54 +0300

    feat: create '28- Factorial with Recursion.py'

---
+++ 140 Basic Programs/28- Factorial with Recursion.py
@@ -0,0 +1,19 @@
+# Write a Python Program to Find Factorial of Number Using Recursion.
+
+
+def factorial(n):
+    if n == 1:
+        return n
+    else:
+        return n * factorial(n - 1)
+
+
+num = int(input("Enter the number: "))
+
+# check if the number is negative
+if num < 0:
+    print("Sorry, factorial does not exist for negative numbers.")
+elif num == 0:
+    print("The factorial of 0 is 1")
+else:
+    print("The factorial of", num, "is", factorial(num))
