5dfdceccf7723d85a172cc3ae03eac4cf4d88fc4
Mustafa Eftekin <75522456+eftekin@users.noreply.github.com>
2024-07-31 22:49:16 +0300

    chore(main.py): rearrange imports and refactor function to improve readability and maintainability feat(main.py): add parsing function 'parse_url' and update bus schedule logic to display bus label

--- side_projects/bus_schedule_streamlit/main.py
+++ side_projects/bus_schedule_streamlit/main.py
@@ -1,41 +1,44 @@
-import requests
-import pandas as pd
-from bs4 import BeautifulSoup
-import streamlit as st
 from datetime import datetime
+
+import pandas as pd
 import pytz
+import requests
+import streamlit as st
+from bs4 import BeautifulSoup
+
+
+def parse_url(url_list):
+    weekdays_list = {}
+
+    for url, label in url_list:
+        data = requests.get(url).text
+        # create BeautifulSoup object
+        soup = BeautifulSoup(data, "html.parser")
+
+        # looking for the table with the class line-table
+        table = soup.find("table", class_="line-table")
+
+        for row in table.tbody.find_all("tr"):
+            # find all data for each column
+            columns = row.find_all("td")
+            weekdays_list.update({columns[0].text.strip(): label})
+    return weekdays_list
+
 
 # Downloading contents of the web page
 urls = [
-    "https://otobussaatleri.net/50l-alibeykoy-metro-besiktas-otobus-saatleri/",
-    "https://otobussaatleri.net/50t-alibeykoy-metro-taksim-otobus-saatleri/",
+    ("https://otobussaatleri.net/50l-alibeykoy-metro-besiktas-otobus-saatleri/", "50L"),
+    ("https://otobussaatleri.net/50t-alibeykoy-metro-taksim-otobus-saatleri/", "50T"),
 ]
-weekdays_list = []
-for url in urls:
-    data = requests.get(url).text
-    # create BeautifulSoup object
-    soup = BeautifulSoup(data, "html.parser")
-
-    # create list with all tables
-    tables = soup.find_all("table")
-
-    # looking for the table with the class line-table
-    table = soup.find("table", class_="line-table")
-
-    # collect data
-    for row in table.tbody.find_all("tr"):
-        # find all data for each column
-        columns = row.find_all("td")
-        weekdays_list.append(columns[0].text.strip())
+weekdays_hour_list = parse_url(urls)
+# sort weekdays list
+weekdays_hour_list_sorted = dict(sorted(weekdays_hour_list.items()))
 
 # get current time
 turkey_timezone = pytz.timezone("Europe/Istanbul")
 utc_now = datetime.utcnow()
 turkey_now = utc_now.replace(tzinfo=pytz.utc).astimezone(tz=turkey_timezone)
 current_time = turkey_now.strftime("%H:%M")
-
-# sort weekdays list
-weekdays_list_sorted = sorted(weekdays_list)
 
 # print title
 page_title = "Weekday Bus Schedule: Alibeyköy to Bomonti (50L and 50T)"
@@ -50,10 +53,11 @@ def convert_to_minutes(time_str):
 
 # print bus schedule
 bus_found = False
-for time in weekdays_list_sorted:
+for time, label in weekdays_hour_list_sorted.items():
     if int(convert_to_minutes(current_time)) <= int(convert_to_minutes(time)):
         st.markdown(
-            f"<h3 style='text-align:center;'>{time}</h3>", unsafe_allow_html=True
+            f"<h3 style='text-align:center; font-size:24px;'>{time} <span style='color:slategray; font-style:italic;'> &ensp; ({label})</span></h3>",
+            unsafe_allow_html=True,
         )
         bus_found = True
 
