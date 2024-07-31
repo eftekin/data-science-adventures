707892354add6c5aba3806620b5d7ee6d5f96822
Mustafa Eftekin <75522456+eftekin@users.noreply.github.com>
2024-07-31 22:48:00 +0300

    fix(app.py): fix indentation and add logic to createImage function feat(app.py): add a last_update variable for the last update time feat(utils.py): add course_code_and_name_column to better identify courses feat(utils.py): format exam dates in format_date function for consistency feat(utils.py): read Excel file for 2024 Bahar Dönemi final exam dates feat(utils.py): create createImage function to generate an image of the result DataFrame

--- side_projects/exam_genius/app.py
+++ side_projects/exam_genius/app.py
@@ -1,35 +1,61 @@
 import streamlit as st
 
-from utils import df, en_createResultDf, tr_createResultDf
+from utils import createImage, df, en_createResultDf, tr_createResultDf
+
+last_update = "10.06.2024 17:55"
 
 st.title("Exam Genius📚")
 
 language_on = st.toggle("🇺🇸 EN", key="language_toggle", value=False)
 
 if language_on:
-    st.write("### 2023 Fall Semester Final Exam Dates")
-    st.write("Below are the final exam dates for the 2023 Fall semester.")
+    st.write("### 2024 Spring Semester Final Exam Dates")
+    st.write("Below are the final exam dates for the 2024 Spring semester.")
     st.write(
         "Please select the course codes of the courses for which you want to see the exam dates."
     )
     course_list = st.multiselect(
-        "Select Courses", df["DERS KODU"].str.upper(), placeholder="Course Code"
-    )  # Ders kodlarını büyük harfe dönüştür
+        "Select Courses",
+        df["DERS KODU VE ADI"],
+        placeholder="Course Code or Name",
+    )
     col1, col2 = st.columns(2)
     if len(course_list) > 0 and col1.button("Show Exam Dates"):
         result_df = en_createResultDf(course_list)
         st.dataframe(result_df, hide_index=True)
-        # col2.download_button("Download Calendar", "main.py")
+        createImage(result_df)
+        with open("output/examgenius.png", "rb") as file:
+            col2.download_button(
+                "Download as Image",
+                data=file,
+                file_name="examgenius.png",
+                mime="image/png",
+            )
+    st.caption(f"Last Update {last_update}")
+    st.caption("For feedback: [mustafa@eftekin.dev](mailto:mustafa@eftekin.dev) 💌")
 
 else:
-    st.write("### 2023 Güz Dönemi Final Sınav Tarihleri")
-    st.write("Aşağıda 2023 Güz dönemi için final sınav tarihleri yer almaktadır.")
+    st.write("### 2024 Bahar Dönemi Final Sınav Tarihleri")
+    st.write("Aşağıda 2024 Bahar dönemi için final sınav tarihleri yer almaktadır.")
     st.write("Lütfen sınav tarihlerini görmek istediğiniz ders kodlarını seçin.")
     course_list = st.multiselect(
-        "Dersleri Seçin", df["DERS KODU"].str.upper(), placeholder="Ders Kodu"
+        "Dersleri Seçin",
+        df["DERS KODU VE ADI"],
+        placeholder="Ders Kodu veya Adı",
     )
     col1, col2 = st.columns(2)
     if len(course_list) > 0 and col1.button("Sınav Tarihlerini Göster"):
         result_df = tr_createResultDf(course_list)
         st.dataframe(result_df, hide_index=True)
-        # col2.download_button("Takvimi İndir", "main.py")
+        createImage(result_df)
+        with open("output/examgenius.png", "rb") as file:
+            col2.download_button(
+                "Resim Olarak İndir",
+                data=file,
+                file_name="examgenius.png",
+                mime="image/png",
+            )
+    st.caption(f"Son Güncelleme {last_update}")
+    st.caption(
+        "Geri bildirimleriniz için: [mustafa@eftekin.dev](mailto:mustafa@eftekin.dev) 💌"
+    )
--- side_projects/exam_genius/utils.py
+++ side_projects/exam_genius/utils.py
@@ -1,21 +1,40 @@
+# Import the necessary libraries
 import datetime
 
 import pandas as pd
+import plotly.figure_factory as ff
 import requests
 from unidecode import unidecode
 
-url = "https://halic.edu.tr/tr/s-duyurular/Documents/2024/01/02/2023-2024-guz-donemi-final-sinav-programi-all-list.xlsx"
+
+# Define the function to format date strings
+def format_date(date_str):
+    try:
+        date_obj = datetime.datetime.strptime(date_str.split(" ")[0], "%Y-%m-%d")
+    except ValueError:
+        try:
+            date_obj = datetime.datetime.strptime(date_str.split(" ")[0], "%d.%m.%Y")
+        except ValueError:
+            return "Invalid date format"
+
+    formatted_date = date_obj.strftime("%d/%m/%Y")
+    return formatted_date
+
+
+# Read the Excel file into a DataFrame
+url = "https://halic.edu.tr/tr/s-duyurular/Documents/2024/06/10/2023-2024-bahar-donemi-final-sinavlari-tum-liste.xlsx"
 midterm_xls = requests.get(url)
 
 df = pd.read_excel(midterm_xls.content)
-
+# Define column names for clarity
 exam_date_column = "SINAV GÜNÜ"
 exam_time_column = "BAŞLANGIÇ SAATİ"
 course_code_column = "DERS KODU"
 course_name_column = "DERS ADI"
+course_code_and_name_column = "DERS KODU VE ADI"
 classroom_code_column = "DERSLİK KODLARI"
 
-
+# Clean up the data in the DataFrame
 df[course_code_column] = df[course_code_column].apply(lambda x: x.split(";")[0])
 df[course_name_column] = df[course_name_column].apply(lambda x: x.split(";")[0])
 df[classroom_code_column] = df[classroom_code_column].apply(lambda x: str(x))
@@ -24,6 +43,7 @@ df[classroom_code_column] = df[classroom_code_column].apply(
 )
 df[course_code_column] = df[course_code_column].apply(lambda y: unidecode(y).lower())
 
+# Select relevant columns and group the data
 df = df[
     [
         exam_date_column,
@@ -57,55 +77,84 @@ df = df[
     ]
 ].sort_values(by=exam_date_column)
 
+# Add a column for course code and name
+df[course_code_and_name_column] = (
+    df[course_code_column].str.upper() + " (" + df[course_name_column] + ")"
+)
+
 course_list = []
 
 
+# Retrieve and format the exam date and time in Turkish
 def tr_getExamDate(course_code):
-    course_code = unidecode(course_code).lower()
-
-    date = df[df[course_code_column] == course_code][exam_date_column].values[0]
+    date = df[df[course_code_and_name_column] == course_code][exam_date_column].values[
+        0
+    ]
     week_day = date.split(" ")[1]
-    date_full = datetime.datetime.strptime(date.split(" ")[0], "%Y-%m-%d").strftime(
-        "%d/%m/%Y"
-    )
+    date_full = format_date(date)
     date = date_full + " " + week_day
-    time = df[df[course_code_column] == course_code][exam_time_column].values[0]
-    return str(date) + " " + str(time)
+    time = df[df[course_code_and_name_column] == course_code][exam_time_column].values[
+        0
+    ]
+    if isinstance(time, str):
+        try:
+            time_obj = datetime.datetime.strptime(time, "%H:%M:%S").time()
+        except ValueError:
+            time_obj = datetime.datetime.strptime(time, "%H:%M").time()
+    else:
+        time_obj = time
+    time_str = time_obj.strftime("%H:%M")
+    return str(date) + " " + time_str
 
 
+# Retrieve and format the exam date and time in English
 def en_getExamDate(course_code):
-    course_code = unidecode(course_code).lower()
-
-    date = df[df[course_code_column] == course_code][exam_date_column].values[0]
-    date_en = datetime.datetime.strptime(date.split(" ")[0], "%Y-%m-%d").strftime(
-        "%d/%m/%Y %A"
+    date = df[df[course_code_and_name_column] == course_code][exam_date_column].values[
+        0
+    ]
+    date_formatted = format_date(date.split(" ")[0])
+    date_en = (
+        date_formatted
+        + " "
+        + datetime.datetime.strptime(date_formatted, "%d/%m/%Y").strftime("%A")
     )
-    date = date_en
+    time = df[df[course_code_and_name_column] == course_code][exam_time_column].values[
+        0
+    ]
+    if isinstance(time, str):
+        try:
+            time_obj = datetime.datetime.strptime(time, "%H:%M:%S").time()
+        except ValueError:
+            time_obj = datetime.datetime.strptime(time, "%H:%M").time()
+    else:
+        time_obj = time
+    time_str = time_obj.strftime("%H:%M")
+    return date_en + " " + time_str
 
-    time = df[df[course_code_column] == course_code][exam_time_column].values[0]
-    return date + " " + time
 
-
+# Retrieve the course name
 def getCourseName(course_code):
-    course_code = unidecode(course_code).lower()
-    name = df[df[course_code_column] == course_code][course_name_column].values[0]
+    name = df[df[course_code_and_name_column] == course_code][
+        course_name_column
+    ].values[0]
     return name
 
 
+# Retrieve and format the classroom information
 def getClassroom(course_code):
-    course_code = unidecode(course_code).lower()
-    classroom = df[df[course_code_column] == course_code][classroom_code_column].values[
-        0
-    ]
-    classroom = classroom.split(",")[:5]
-    classroom = ",".join(str(element) for element in classroom)
+    classroom = df[df[course_code_and_name_column] == course_code][
+        classroom_code_column
+    ].values[0]
+    if len(classroom.split(",")) > 5:
+        classroom = classroom.split(",")[:5]
+        classroom = ",".join(str(element) for element in classroom) + "..."
     return classroom
 
 
+# Create a DataFrame in English
 def en_createResultDf(course_list):
     result_df_en = pd.DataFrame(
-        [],
-        columns=["Course Name", "Exam Date", "Classroom Codes"],
+        [], columns=["Course Name", "Exam Date", "Classroom Codes"]
     )
     for course in course_list:
         list_row = [getCourseName(course), en_getExamDate(course), getClassroom(course)]
@@ -114,13 +163,25 @@ def en_createResultDf(course_list):
     return result_df_en
 
 
+# Create a DataFrame in Turkish
 def tr_createResultDf(course_list):
-    result_df_tr = pd.DataFrame(
-        [],
-        columns=["Ders Adı", "Sınav Tarihi", "Sınıf"],
-    )
+    result_df_tr = pd.DataFrame([], columns=["Ders Adı", "Sınav Tarihi", "Sınıf"])
     for course in course_list:
         list_row = [getCourseName(course), tr_getExamDate(course), getClassroom(course)]
         result_df_tr.loc[len(result_df_tr)] = list_row
     result_df_tr = result_df_tr.sort_values("Sınav Tarihi")
     return result_df_tr
+
+
+# Create an image of the result DataFrame
+def createImage(df):
+    course_list = list(df.iloc[:, 0])
+    scale = 21
+    width = max([len(course_name) * scale for course_name in course_list])
+    fig = ff.create_table(df)
+    if width > 800:
+        fig.layout.width = width
+    else:
+        fig.layout.width = 800
+    fig.update_layout(autosize=True)
+    fig.write_image("output/examgenius.png", scale=2)
