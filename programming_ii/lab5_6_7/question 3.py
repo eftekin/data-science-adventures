9c356ee67100e97429c8f25ed726e3416a4c2a3b
Mustafa Eftekin <75522456+eftekin@users.noreply.github.com>
2024-08-11 18:29:28 +0300

    chore(lab): remove outdated/excess code files and notes from previous labs.

--- programming_ii/lab5_6_7/question 3.py
+++
@@ -1,9 +0,0 @@
-animalList = ["cat", "dog", "bird", "turtle", "penguin", "crocodile"]
-
-longest = ""
-for word in animalList:
-    if len(word) > len(longest):
-        longest = word
-
-# print the result
-print("The longest word in the list is:", longest)
--- programming_ii/lab5_6_7/question 6.py
+++
@@ -1,4 +0,0 @@
-
-courses = ("Python Tutorials", "C Tutorials", "Java Tutorials")
-num_courses = len(courses)
-print(num_courses)
